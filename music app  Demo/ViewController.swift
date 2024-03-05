b9d75146edeae0a86618d99da9b7eaf90c5d4460
Timmy <timmyliu1997@hotmail.com>
2024-03-05 23:11:01 +0800

    Initial Commit

---
+++ music app  Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  music app  Demo
+//
+//  Created by Timmy on 2024/3/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
